5e67eedaf4337f640bfafdb0da65ae6146747828
BelakJG <74761299+BelakJG@users.noreply.github.com>
2026-03-10 14:37:43 -0400

    Added mailer function for flight bookings

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,2 +1,13 @@
 class PassengerMailer < ApplicationMailer
+  default from: "bookings@example.com"
+
+  def confirm_email
+    @booking = params[:booking]
+    @flight = @booking.flight
+    @passengers = @booking.passengers
+    @url = "http://example.com/login"
+    @passengers.each do |passenger|
+      mail(to: passenger.email, subject: "Your flight is booked")
+    end
+  end
 end
